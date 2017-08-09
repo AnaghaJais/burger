5a52752ddaed3754e3835fcd83f90eee549bfc04
AnaghaJais <anagha.babu1@gmail.com>
2017-08-08 20:54:38 -0500

    added

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ var connection;
 if(process.env.JAWSDB_URL){
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
- connection = mysql.createConnection({  
+ connection = mysql.createConnection({ 
+   port:3306, 
   host: "localhost",
   user: "root",
   password: "",
@@ -13,13 +14,6 @@ connection = mysql.createConnection(process.env.JAWSDB_URL);
 };
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
